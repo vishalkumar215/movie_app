846de15e1e812ef9d9f1e088b82a2e49cc5c9fb6
vishalkumar215 <vishalchoubey2@gmail.com>
2022-05-18 19:56:42 +0530

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,10 @@ export class Banner extends Component {
     let moviesElem = movies.results[Math.floor((Math.random() * 10) + 1)]
       let backDrop =  moviesElem.backdrop_path
     let release = moviesElem.release_date
+    let overview = moviesElem.overview
+
     return (
+
      
       <div className="card  banner-card">
         <img
@@ -19,7 +22,9 @@ export class Banner extends Component {
         <h5 className="card-title banner-title"><strong> {moviesElem.title}</strong></h5>
         <h6 className="movie-release" style={{marginTop:'0.2rem'}}>Release Date :-{release}</h6>
         <p className="card-text banner-text">
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. At omnis, error animi dolorum et doloremque nam, laboriosam porro id nostrum, qui eveniet praesentium perferendis itaque totam. Tempore corrupti assumenda, nostrum corporis quibusdam beatae atque modi facere a odio sequi recusandae architecto perspiciatis nam laboriosam iure totam amet sunt cum labore.
+        Overview :-
+        <br />
+            {overview}
         </p>
         
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,17 @@ import {Link} from 'react-router-dom'
 export class Navbar extends Component {
   render() {
     return (
-      <div style={{display:'flex', padding:'0.5', color:'maroon'}}>
-        <Link style={{ textDecoration:'none', color:'maroon'}} to ='/'> <h1>Movie App</h1></Link>
+      <>
+           <div  className ="navigation"style={{display:'flex', padding:'0.5', color:'maroon'}}>
+         <Link style={{ textDecoration:'none', color:'maroon'}} to ='/'> <h1>Movie App</h1></Link>
         <Link style={{textDecoration:'none'}} to ='/favourites'><h2 style={{ marginLeft:'2rem', marginTop:'0.5rem',color:'maroon' }}>Favourites</h2></Link>
-        <hr />
+        
          
           
-      </div>
+       </div>
+       
+       </>
+ 
     )
   }
 }
